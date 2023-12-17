a958eadec60f52cd6e32e2ce1775a07083d6b5d6
JiangWuyan <30773833+JiangWuyan@users.noreply.github.com>
2023-12-17 13:55:42 +0000

    去除head

--- app/page.js
+++ app/page.js
@@ -1,21 +1,15 @@
-import Head from 'next/head'
 import Link from 'next/link'
 
 export default function Page() {
   return (
-    <>
-      <Head>
-        <title>To The Sea | 出海新途</title>
-      </Head>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/whois">
-              域名查询
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </>
+    <nav>
+      <ul>
+        <li>
+          <Link href="/whois">
+            域名查询
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
