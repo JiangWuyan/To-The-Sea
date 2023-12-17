adc25b436efccc817ad0f766b55a825e429b4673
JiangWuyan <30773833+JiangWuyan@users.noreply.github.com>
2023-12-17 14:01:18 +0000

    最简

--- app/page.js
+++ app/page.js
@@ -2,14 +2,6 @@ import Link from 'next/link'
 
 export default function Page() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/whois">
-            域名查询
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <Link href="/whois">域名查询</Link>
   );
 }
